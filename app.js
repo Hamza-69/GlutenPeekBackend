a0581d20ff59a635d2459b78c9ae300413e5df6c
Hamza El Rachdi <hamzarachidi04@gmail.com>
2025-06-01 05:33:05 +0300

    Add logic to connect to mongo db

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const mongoose = require('mongoose')
+const env = require('./utils/config')
+const mongoUrl = env.MONGODB_URI
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(mongoUrl).then(() => {
+  console.log('connected to MongoDB')
+}).catch((error) => {
+  console.error('error connecting to MongoDB:', error.message)
+})
 
 app.use(express.json())
 
